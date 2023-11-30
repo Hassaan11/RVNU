b0000419acb5c4147b345775caaf81051062fafb
Hassaan11 <saad117218@gmail.com>
2023-12-01 01:49:33 +0500

    Designed Signup page

--- src/screens/Signup/Signup.jsx
+++ src/screens/Signup/Signup.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from '../../components/header/header';
+import CustomInput from '../../components/input/input';
+import {View, StyleSheet, Text} from 'react-native';
+import CustomButton from '../../components/button/button';
 
-const Signup = () => {
-  return <Header title="HELLO" />;
+const Signup = ({navigation}) => {
+  const [text, onChangeText] = useState('');
+  return (
+    <View style={{flex: 1}}>
+      <View style={{flex: 0.5}}>
+        <Header title="Choose your username and make it memorable." />
+      </View>
+      <View style={styles.view}>
+        <View>
+          <CustomInput
+            text={text}
+            setText={onChangeText}
+            placeholder="Enter username"
+          />
+          <CustomButton
+            text="Continue"
+            onPress={() => {
+              navigation.navigate('Payment');
+            }}
+          />
+        </View>
+        <Text style={styles.text}>Clain your username.</Text>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    backgroundColor: 'black',
+    paddingTop: 20,
+    paddingHorizontal:20,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 50,
+    fontFamily: 'BR shape',
+    color: 'white',
+  },
+});
 
 export default Signup;

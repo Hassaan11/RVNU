350d5fac9f64ff6e85709c0af6c47abff896d81a
Hassaan11 <saad117218@gmail.com>
2023-12-01 01:50:35 +0500

    Setup navigation and app

--- App.jsx
+++ App.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  Platform,
-} from 'react-native';
+import {SafeAreaView, Platform, StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import Signup from './src/screens/Signup/Signup';
+import Payment from './src/screens/Payment/Payment';
+import {PaperProvider} from 'react-native-paper';
+import ProductIntroduced from './src/screens/Product-introduced/Product-introduced';
+import PaymentSuccessful from './src/screens/Payment-successful/Payment-successful';
 
 const Stack = createStackNavigator();
-
 
 const App = () => {
   return (
     <SafeAreaView
-      style={{flex: 1, paddingTop: Platform.OS === 'android' && 30}}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Signup" component={Signup} />
-          {/* <Stack.Screen name="Painting" component={Painting} /> */}
-        </Stack.Navigator>
-      </NavigationContainer>
+      style={{
+        flex: 1,
+        paddingTop: Platform.OS === 'android' && 20,
+        backgroundColor: 'black',
+      }}>
+      <StatusBar barStyle={'light-content'} />
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Signup" component={Signup} />
+            <Stack.Screen name="Payment" component={Payment} />
+            <Stack.Screen name="ProductIntroduced" component={ProductIntroduced} />
+            <Stack.Screen name="PaymentSuccessful" component={PaymentSuccessful} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
     </SafeAreaView>
   );
 };

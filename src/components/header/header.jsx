73c21ef53729afc108cc54c913c09d3d3a923568
Hassaan11 <saad117218@gmail.com>
2023-12-01 01:49:18 +0500

    Setup Custom Header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import {Image, View} from 'react-native';
+import {Image, View, Text, StyleSheet} from 'react-native';
 
 const Header = ({title}) => {
   return (
-    <View>
-      <Image source={{uri: ''}} />
-      <Text>{title}</Text>
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/images/logo.png')}
+        style={{width: 30, height: 30}}
+      />
+      {title === 'Payment Successful' ? (
+        <Text style={styles.title}>{title} {"\n"}
+        <Text style={{color:'#F9C6AD'}}> chris</Text> earnt £15.00
+        </Text>
+      ) : (
+        <Text style={styles.title}>{title}</Text>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 60,
+  },
+  title: {
+    marginTop: 30,
+    fontFamily: 'BR shape',
+    textAlign: 'center',
+    fontSize: 18,
+    width: '80%',
+    color:'black'
+    // DM Sans
+  },
+});
 
 export default Header;
